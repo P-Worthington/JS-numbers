1920310eff3d9081d7054571fe0011e5a88e939a
Paul Worthington <pworthington17@gmail.com>
2024-07-04 12:21:36 +0000

    calc

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,10 +1,35 @@
 function getValue(a) {
     var x = document.getElementById("myForm").elements[a].value;
-    return x;
-  };
+    x = Number(x);
+    if (isNaN(x) === true) {
+        alert('Please input a number');
+    } else {
+        return x;
+    };
+};
 
 function additionOperation (a, b) {
     result = a + b;
+    return result;
+};
+
+function subtractionOperation (a, b) {
+    result = a - b;
+    return result;
+};
+
+function multiplicationOperation (a, b) {
+    result = a * b;
+    return result;
+};
+
+function divisionOperation (a, b) {
+    result = a / b;
+    return result;
+};
+
+function exponentialOperation (a, b) {
+    result = a ** b;
     return result;
 };
 
@@ -14,3 +39,31 @@ function addition () {
     result = additionOperation (value1, value2);
     document.getElementById("query").innerHTML = `${value1} + ${value2} = ${result}`;
 };
+
+function subtraction () {
+    value1 = getValue(0);
+    value2 = getValue(1);
+    result = subtractionOperation (value1, value2);
+    document.getElementById("query").innerHTML = `${value1} - ${value2} = ${result}`;
+}
+
+function multiplication () {
+    value1 = getValue(0);
+    value2 = getValue(1);
+    result = multiplicationOperation (value1, value2);
+    document.getElementById("query").innerHTML = `${value1} x ${value2} = ${result}`;
+}
+
+function division () {
+    value1 = getValue(0);
+    value2 = getValue(1);
+    result = divisionOperation (value1, value2);
+    document.getElementById("query").innerHTML = `${value1} / ${value2} = ${result}`;
+}
+
+function exponential () {
+    value1 = getValue(0);
+    value2 = getValue(1);
+    result = exponentialOperation (value1, value2);
+    document.getElementById("query").innerHTML = `${value1} ^ ${value2} = ${result}`;
+}
